c23014352d8a9399a241bf88245f9bf39fe065db
mtianyan <1147727180@qq.com>
2020-12-01 23:51:53 +0800

    remove .umi

--- demos/tyadmin_demo_finish/tyadmin/src/.umi/plugin-dva/exports.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-
-export { connect, useDispatch, useStore, useSelector } from '/Users/mtianyan/tyRepos/tyadmin_api_cli/demos/tyadmin_demo_finish/tyadmin/node_modules/dva';
-export { getApp as getDvaApp } from './dva';
